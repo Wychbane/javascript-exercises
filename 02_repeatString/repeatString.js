fe0093f200e1469c579a893aef3efaed20ad19db
Wychbane <davemurphy81@hotmail.com>
2022-07-28 13:56:59 +0100

    Complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(phrase, num) {
+    let word = ''
+    let i = 0
+    if (num < 0){
+        return 'ERROR'
+    }
+    else while (i < num ) {
+        word += phrase;
+        i++;
+    }
+    return word
 
 };
 
