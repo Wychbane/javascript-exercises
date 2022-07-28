95d245fa4e827e50097034e8143268debc63885b
Wychbane <davemurphy81@hotmail.com>
2022-07-28 15:06:43 +0100

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = ''
+    let length = string.length
+    for (i = length ; i >= 0 ; i--){
+        letter = string.slice(i-1,i);
+        reverse += letter;
+    }
+    return reverse;
 };
 
 // Do not edit below this line
