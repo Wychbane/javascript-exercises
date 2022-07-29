d20b9b232ceac168f1669a1f23c4305b6daddb1d
Wychbane <davemurphy81@hotmail.com>
2022-07-29 13:34:45 +0100

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+// Loop through numbers from a to b inclusive
+// add each number to previous number
+const sumAll = function(a, b) {
+    // check which is higher a or b
+    let sum = 0;
+    let upper = 0;
+    let lower = 0;
+    // Check if any of the numbers are negative or not an integer
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    } 
+    if (a > b) {
+      upper = a;
+      lower = b;
+    } else {
+      upper = b;
+      lower = a;
+    }
+    for (let i = lower; i <= upper; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
