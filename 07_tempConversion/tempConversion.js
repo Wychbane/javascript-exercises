45b76ecf18ad5f9dc1c4af8ebc670a5731c3bc04
Wychbane <davemurphy81@hotmail.com>
2022-07-29 14:22:21 +0100

    Add other solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -11,6 +11,18 @@ const ctof = function(deg) {
   return Number((((deg * 9/5) + 32)).toFixed(1));
 };
 
+/*
+
+const ftoc = function(f) {
+ return Math.round((f - 32) * (5/9) * 10) / 10;
+};
+
+const ctof = function(c) {
+  return Math.round(((c * 9/5) + 32) * 10) / 10;
+};
+
+*/
+
 // Do not edit below this line
 module.exports = {
   ftoc,
