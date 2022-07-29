844aba5d68b35d3aad2ebcb38862e3c8f47f9578
Wychbane <davemurphy81@hotmail.com>
2022-07-29 14:20:34 +0100

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+/*
+[°F] = [°C] × 9⁄5 + 32
+[°C] = ([°F] − 32) × 5⁄9
+*/
 
+const ftoc = function(deg) {
+  return Number(((deg - 32) * (5/9)).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(deg) {
+  return Number((((deg * 9/5) + 32)).toFixed(1));
 };
 
 // Do not edit below this line
