d710834b7dc726606de4129e61160b71cba705fa
Wychbane <davemurphy81@hotmail.com>
2022-07-29 11:17:55 +0100

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
 
+/* 
+Loop through the array.
+check for number at each position
+if target number present splice the number out
+
+For 
+*/
+const removeFromArray = function(arrayObject, ...objToRemove) {
+    const array = arrayObject;
+    const arrayOfObjs = objToRemove;
+    // Loop through the array of objects to check
+    for (int in arrayOfObjs){
+        // Use the object found at each position
+        let obj = arrayOfObjs[int]
+        for (object in array) {
+            if (array[object] === obj) {
+                array.splice(object, 1);
+            };
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
