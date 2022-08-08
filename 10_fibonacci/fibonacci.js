894cad39341d739443e71c1247bdc0126c85c99f
Wychbane <davemurphy81@hotmail.com>
2022-08-08 16:54:17 +0100

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) { 
+    if (typeof(n) === 'string'){
+        n = Number(n);
+    }
+    if (n < 1 ) {
+        return 'OOPS';
+    }
+    else if (n === 1){
+        return 1;
+    } else if (n === 2){
+        return 1;
+    }
+    return (fibonacci(n-2) + fibonacci(n-1));
 };
 
 // Do not edit below this line
