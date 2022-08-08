ba600934fdfb30c2d3f4b4cac13df0371c9e93da
Wychbane <davemurphy81@hotmail.com>
2022-08-08 13:27:28 +0100

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a ,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length ?
+  array.reduce((firstItem, nextItem) => firstItem * nextItem) : 0;
 };
 
-const power = function() {
-	
+const power = function(int, ord) {
+  return int**ord;
+  //return Math.pow(int, ord);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+	let total = 1;
+  for (let i= num; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
+const recursiveFactorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * recursiveFactorial(n-1);
+};
+
 
 // Do not edit below this line
 module.exports = {
