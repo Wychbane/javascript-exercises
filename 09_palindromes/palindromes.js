2331d03dce0a1b7a9fd2d5bceabadd34d7554a52
Wychbane <davemurphy81@hotmail.com>
2022-08-08 15:40:47 +0100

    Add Odin solution as comment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -6,5 +6,17 @@ const palindromes = function (string) {
     return stripped === reverseString
  };
 
+ /*
+const palindromes = function(string) {
+  processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+  return (
+    processedString
+      .split("")
+      .reverse()
+      .join("") == processedString
+  );
+};
+ */
+
 // Do not edit below this line
 module.exports = palindromes;
