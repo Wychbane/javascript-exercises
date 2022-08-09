64983990e3d006981df31a2278d23207a7ad8438
Wychbane <davemurphy81@hotmail.com>
2022-08-09 11:03:29 +0100

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    return people.reduce((oldest, next) => {
+     const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+     const nextAge = getAge(next.yearOfBirth, next.yearOfDeath);
+     return oldestAge > nextAge ? oldest : next;
+    });
+   };
+const getAge = function (birth, death){
+    if (!death){
+        death = new Date().getFullYear;
+    }
+    return death - birth;
+    }
 
 // Do not edit below this line
 module.exports = findTheOldest;
